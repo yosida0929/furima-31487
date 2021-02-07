82735ada09ed078451081a5f4015cbd3ad7fc27b
yosida0929 <xqkkr256@gmail.com>
2021-02-07 14:39:36 +0900

    ユーザー管理機能1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :first_name,          presence: true
-  validates :name,                presence: true
-  validates :first_name_kana,     presence: true
-  validates :name_kana,           presence: true
-  validates :birth_day,           presence: true
+  with_options presence: true do
+    validates :nickname,            presence: true
+    validates :first_name,          presence: true
+    validates :name,                presence: true
+    validates :first_name_kana,     presence: true
+    validates :name_kana,           presence: true
+    validates :birth_day,           presence: true
+  end
+      with_options presence: true, format: { with: /\A[a-z\d]+\z/i, message: '半角英数字を使用してください' } do
+        validates :password,          presence: true
+    end
 
-  has_many :items
-  has_many :buyers
+    has_many :items
+    has_many :buyers
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+    end
+    it "emailが空では登録できない" do
+    end
+    it "first_nameが空では登録できない" do
+    end
+    it "nameが空では登録できない" do
+    end
+    it "first_name_kanaが空では登録できない" do
+    end
+    it "name_kanaが空では登録できない" do
+    end
+    it "birth_dayが空では登録できない" do
+    end
+  end
+end
