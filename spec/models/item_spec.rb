ab3b9389eec2525549f2daca95daa5b6efff410a
yosida0929 <xqkkr256@gmail.com>
2021-02-11 11:30:21 +0900

    モデル単体テストコード3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
     end
 
-    it "10000000円以下だと登録できない" do
+    it "10000000円以上だと登録できない" do
       @item.price = 10000001
       @item.valid?
       expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
